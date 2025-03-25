383bde26fd5b9c36be7b82c302659a08a49d4b87
Core <daverencordero@gmail.com>
2025-03-25 15:54:33 +0800

    chore: minor optimizations

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@noos/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
